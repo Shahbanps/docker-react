9ad1614cf5f33e3a4565e096605af464b5a3eb9f
Shahbanps <psshhaban@gmail.com>
2022-10-29 18:18:02 +0530

    added expose 80 second time

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Iwas tehre
+          Hi there how are you!
         </p>
         <a
           className="App-link"
